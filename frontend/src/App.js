746cb928a36664b624c85bcd73acc6a0384ddf41
Chris Qin <chriscode3@Chriss-MBP.fios-router.home>
2019-10-20 17:53:49 -0400

    test

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Test if this is working
         </a>
       </header>
     </div>
